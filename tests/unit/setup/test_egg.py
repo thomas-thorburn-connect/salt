4e8b389ba44c978cbfb7237e58cce853e9811b7c
Pedro Algarvio <pedro@algarvio.me>
2020-04-29 10:13:55 +0100

    Move setup tests to unit tests. Adjust.

--- tests/unit/setup/test_egg.py
+++ tests/unit/setup/test_egg.py
@@ -4,68 +4,60 @@ tests.integration.setup.test_egg
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-# Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 import re
-import shutil
 
-# Import salt libs
 import salt.utils.path
 import salt.utils.platform
 from salt.modules.virtualenv_mod import KNOWN_BINARY_NAMES
-from tests.support.case import ModuleCase
-from tests.support.helpers import VirtualEnv, destructiveTest
-
-# Import Salt Testing libs
+from tests.support.helpers import VirtualEnv, with_tempdir
 from tests.support.runtests import RUNTIME_VARS
-from tests.support.unit import skipIf
+from tests.support.unit import TestCase, skipIf
 
 
-@destructiveTest
 @skipIf(
     salt.utils.path.which_bin(KNOWN_BINARY_NAMES) is None, "virtualenv not installed"
 )
-class EggSetupTest(ModuleCase):
+class EggSetupTest(TestCase):
     """
     Tests for building and installing egg packages
     """
 
-    def setUp(self):
-        # ensure we have a clean build dir
-        self._clean_build()
-
-    def _clean_build(self):
-        """
-        helper method to clean the build dir
-        """
-        dirs = [
-            os.path.join(RUNTIME_VARS.CODE_DIR, "build"),
-            os.path.join(RUNTIME_VARS.CODE_DIR, "salt.egg-info"),
-            os.path.join(RUNTIME_VARS.CODE_DIR, "dist"),
-        ]
-        for _dir in dirs:
-            if os.path.exists(_dir):
-                shutil.rmtree(_dir)
-
     @skipIf(True, "SLOWTEST skip")
-    def test_egg_install(self):
+    @with_tempdir()
+    def test_egg_install(self, tempdir):
         """
         test installing an egg package
         """
         # Let's create the testing virtualenv
         with VirtualEnv() as venv:
-            ret = self.run_function(
-                "cmd.run",
-                [
-                    "{0} setup.py install --prefix={1}".format(
-                        venv.venv_python, venv.venv_dir
-                    )
-                ],
+            venv.run(venv.venv_python, "setup.py", "clean", cwd=RUNTIME_VARS.CODE_DIR)
+
+            # Setuptools installs pre-release packages if we don't pin to an exact version
+            # Let's download and install requirements before, running salt's install test
+            venv.run(
+                venv.venv_python,
+                "-m",
+                "pip",
+                "download",
+                "--dest",
+                tempdir,
+                RUNTIME_VARS.CODE_DIR,
+            )
+            for fname in os.listdir(tempdir):
+                venv.install(os.path.join(tempdir, fname))
+
+            venv.run(
+                venv.venv_python,
+                "setup.py",
+                "install",
+                "--prefix",
+                venv.venv_dir,
                 cwd=RUNTIME_VARS.CODE_DIR,
             )
-            self._clean_build()
+            venv.run(venv.venv_python, "setup.py", "clean", cwd=RUNTIME_VARS.CODE_DIR)
             lib_dir = os.path.join(venv.venv_dir, "lib")
             for _dir in os.listdir(lib_dir):
                 site_pkg = os.path.join(lib_dir, _dir, "site-packages")
@@ -78,7 +70,13 @@ class EggSetupTest(ModuleCase):
                         break
 
             # Let's ensure the version is correct
-            pip_ver = self.run_function("pip.list", bin_env=venv.venv_dir).get("salt")
+            cmd = venv.run(venv.venv_python, "-m", "pip", "freeze")
+            for line in cmd.stdout.splitlines():
+                if line.startswith("salt"):
+                    pip_ver = line.split("=")[-1]
+                    break
+            else:
+                self.fail("Salt was not found installed")
             egg_ver = [
                 x for x in egg.split("/")[-1:][0].split("-") if re.search(r"^\d.\d*", x)
             ][0]
